469f80e474e4855320b5be5c0dc24a566d8643c0
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-06-02 10:38:15 +0530

    Update HttpTriggerJava.java

--- src/main/java/org/rosemi/lovelock/HttpTriggerJava.java
+++ src/main/java/org/rosemi/lovelock/HttpTriggerJava.java
@@ -27,8 +27,8 @@ public class HttpTriggerJava {
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + reverseName).build();
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + reverseName + " Good morning").build();
         }
-        
+
     }
 }
